1d8883bfc8da81699f4ae87180affa4a26e27662
KevinBatres <44206241+KevinBatres@users.noreply.github.com>
2020-02-06 12:17:12 -0700

    2st commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * 5f * Time.deltaTime * Axis.x);  
+    }
+}
